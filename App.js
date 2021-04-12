a7cca4f7176761f35f0f072238760b2ec911efc2
Mark Alindogan <marleoali_777@yahoo.com>
2021-04-12 18:17:26 -0500

    Forms

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main from './components/MainComponent';
-//console.disableYellowBox = true;
+console.disableYellowBox = true;
 
 export default function App() {
     return (

9412d8bfacd86aed04fdcaa68bd9ad4f7163e0ae
Mark Alindogan <marleoali_777@yahoo.com>
2021-04-13 18:33:55 -0500

    using redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, FlatList } from 'react-native';
+import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission() {
     return(
@@ -19,12 +27,6 @@ function Mission() {
 };
 
 class About extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        }
-    }
 
     static navigationOptions = {
         title: 'About Us'
@@ -37,7 +39,7 @@ class About extends Component{
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
@@ -45,14 +47,15 @@ class About extends Component{
             <ScrollView>
                 <Mission/>
                 <Card title='Community Partners'>
-                    <FlatList 
-                    data={this.state.partners} 
-                    renderItem={renderPartner}
-                    keyExtractor={item => item.id.toString()} />
+                <FlatList 
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item => item.id.toString()}
+                    />
                 </Card>
             </ScrollView>
         );
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);

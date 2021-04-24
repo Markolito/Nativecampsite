c2341340e6f309315203ab98ef832107eea8affa
Mark Alindogan <marleoali_777@yahoo.com>
2021-04-24 11:21:02 -0500

    week 3 workshop assignment

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -42,31 +42,6 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
     setTimeout(() => {
         dispatch(addComment(newComment));
     }, 2000);
-
-    // return fetch(baseUrl + 'comments', {
-    //         method: "POST",
-    //         body: JSON.stringify(newComment),
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         }
-    //     })
-    //     .then(response => {
-    //             if (response.ok) {
-    //                 return response;
-    //             } else {
-    //                 const error = new Error(`Error ${response.status}: ${response.statusText}`);
-    //                 error.response = response;
-    //                 throw error;
-    //             }
-    //         },
-    //         error => { throw error; }
-    //     )
-    //     .then(response => response.json())
-    //     .then(response => dispatch(addComment(response)))
-    //     .catch(error => {
-    //         console.log('post comment', error.message);
-    //         alert('Your comment could not be posted\nError: ' + error.message);
-    //     });
 }
 
 export const addComment = comment => ({

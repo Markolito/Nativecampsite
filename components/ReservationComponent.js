8d3fb4e4627ede4ea8e6b5c0d01ec61aa4f8e4da
Mark Alindogan <marleoali_777@yahoo.com>
2021-04-25 15:46:00 -0500

    local notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -34,6 +35,32 @@ class Reservation extends Component {
 
     static navigationOptions = {
         title: 'Reserve Campsite'
+    }
+    
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
@@ -103,7 +130,10 @@ class Reservation extends Component {
                                     },
                                     {
                                         text: 'Search',
-                                        onPress: () => this.resetForm()
+                                        onPress: () => {
+                                            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                                            this.resetForm()
+                                        }
                                     },
                                 ],
                                 { cancelable: false }
